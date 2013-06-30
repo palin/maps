68997d549fe499faf4fc517aa72b2e1f7bc45011
Maciej Paliński <maciej.palinski@netguru.pl>
2013-06-30 22:05:30 +0200

    improved spec homecontroller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 
 describe HomeController do
   describe "#index" do
-    it "responses with 200 OK" do
-      response.code.should == "200"
-    end
+    subject { get :index }
+
+    its(:code) { should == "200" }
   end
 end

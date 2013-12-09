caf7cbf9b1a6b0b569e4738f8acccc9b73272e52
Maciej Palinski <maciekpalinski@gmail.com>
2013-12-09 22:00:31 +0000

    try to fix fonts on staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,10 @@ Reporter::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w(.svg .eot .woff .ttf)
+
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 

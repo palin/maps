ea3826b5b247191772aed26dde0f23f7391845b3
Maciej Palinski <maciekpalinski@gmail.com>
2013-12-12 22:03:26 +0000

    use translations

--- app/models/abuse.rb
+++ app/models/abuse.rb
@@ -6,17 +6,6 @@ class Abuse < ActiveRecord::Base
   validates_presence_of :name, :report_id
 
   def reason
-    case name
-    when "spam"
-      "Spam"
-    when "duplicate"
-      "Duplikat"
-    when "content"
-      "Nieodpowiednia treść"
-    when "solved"
-      "Problem rozwiązano"
-    else
-      "Brak powodu"
-    end
+    I18n.t("models.abuse.#{name}")
   end
 end

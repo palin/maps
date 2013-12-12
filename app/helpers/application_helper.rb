1926ad66e3e323aebcc05655dacb5a0cd4c2703f
Maciej Palinski <maciekpalinski@gmail.com>
2013-12-12 20:55:43 +0000

    use new syntax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column(model) ? "current #{sort_direction}" : nil
     direction = column == sort_column(model) && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, { sort: column, direction: direction }, { class: css_class }
   end
-
 end

c98cea9ae04f88f55a19b0432164273a9a18932a
Maciej Palinski <maciekpalinski@gmail.com>
2013-12-17 20:46:59 +0000

    use env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Reporter::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_controller.asset_host = "//fixer-photos-prod.s3.amazonaws.com"
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH

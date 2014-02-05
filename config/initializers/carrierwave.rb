0e6681fcbd4348d202669689f02da739a8f7607a
Maciej Palinski <maciekpalinski@gmail.com>
2014-02-05 22:44:37 +0000

    recreate aws keys and hide in server config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAJZ6VLEWVTUNQOUUA",
-      :aws_secret_access_key  => "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",
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
       :region                 => 'eu-west-1'
     }
     config.fog_directory  = s3_config['bucket']
